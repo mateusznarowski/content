375441895227b3b2de13e2fe0a7ccd5137a86a41
Mateusz Narowski <mateusz.narowski@mndev.eu>
2025-01-28 19:44:30 +0100

    Added @nuxt/content and @nuxthub/core

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,8 +1,25 @@
 <template>
-  <div>
-    <pre>page...</pre>
-  </div>
+  <ContentRenderer
+    v-if="page"
+    :value="page"
+  />
 </template>
 
 <script setup lang="ts">
+import { withLeadingSlash } from 'ufo'
+import { computed } from 'vue'
+
+import { createError, queryCollection, useAsyncData, useRoute } from '#imports'
+
+const route = useRoute()
+
+const slug = computed(() => withLeadingSlash(String(route.params.slug).replace(/,/g, '/')))
+
+const { data: page } = useAsyncData('page' + slug.value, () => {
+  return queryCollection('content').path(slug.value).first()
+})
+
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+}
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,25 @@
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/content',
+    '@nuxthub/core'
+  ],
   components: { dirs: [] },
   imports: { autoImport: false },
   devtools: { enabled: true },
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    },
+    database: {
+      type: 'd1',
+      binding: 'DB'
+    }
+  },
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2025-01-01',
+  hub: {
+    database: true
+  },
   eslint: { config: { stylistic: true } }
 })
